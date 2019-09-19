a509473f8925ef639b5811ab841e721d65c7fb69
Michael Lynch <michael@michaelrlynch.com>
2019-09-19 14:47:56 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,17 @@
 class Author
   attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
 end
